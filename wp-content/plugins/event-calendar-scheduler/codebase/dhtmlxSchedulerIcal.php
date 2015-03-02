2cb568a007f2c6898f16470495c0b63c329f6de3
Jordan Graham <jordan@handsomedogstudio.com>
2015-03-02 16:01:06 -0600

    Removed plugins 'event calendar scheduler', 'hello dolly' and 'wordpress importer'.

--- wp-content/plugins/event-calendar-scheduler/codebase/dhtmlxSchedulerIcal.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!isset($_POST['data'])) die();
-header('Content-type: text/calendar; charset=utf-8');
-header('Content-Disposition: inline; filename=scheduler.ics');
-echo $_POST["data"];
-?>
--- wp-content/plugins/event-calendar-scheduler/codebase/locale/common_en.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$scheduler_locale = Array(
-	'Scheduler' => 'Scheduler',
-	'Oncoming events' => 'Oncoming events',
-	'Installation error: Event Calendar / Scheduler plugin requires PHP 5.x' => 'Installation error: Event Calendar / Scheduler plugin requires PHP 5.x'
-);
-
-?>
